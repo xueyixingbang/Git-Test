0774a3a6bb5a17a9ae4030f81f5b39c59d89122a
Bryan <xueyixingbang@gmail.com>
2014-04-16 21:34:54 +0800

    added a test class

---
+++ Git Test/DDTest.h
@@ -0,0 +1,13 @@
+//
+//  DDTest.h
+//  Git Test
+//
+//  Created by 杜松 on 14-4-16.
+//  Copyright (c) 2014年 Humble Education. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDTest : NSObject
+
+@end
---
+++ Git Test/DDTest.m
@@ -0,0 +1,13 @@
+//
+//  DDTest.m
+//  Git Test
+//
+//  Created by 杜松 on 14-4-16.
+//  Copyright (c) 2014年 Humble Education. All rights reserved.
+//
+
+#import "DDTest.h"
+
+@implementation DDTest
+
+@end
